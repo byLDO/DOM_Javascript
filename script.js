002347e77be6ffd6c140a178fb4b1008f815cbda
byLDO <byhelido4@gmail.com>
2023-01-11 21:22:15 -0300

    Aula 5
    Função copia entrada

---
+++ script.js
@@ -0,0 +1,10 @@
+var entrada = document.getElementById("entrada");
+var botaoEnviar = document.getElementById("enviar");
+var saida = document.getElementById("saida")
+
+function copiarEntrada(){
+    saida.textContent = entrada.value;
+}
+
+botaoEnviar.addEventListener("click", copiarEntrada);
+
